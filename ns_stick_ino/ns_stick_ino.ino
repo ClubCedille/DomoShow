b43bfa022836283506a35f74023fa6c715d4d837
Nicolas Nadeau <niconadeau0@gmail.com>
2016-01-30 11:56:25 -0500

    The Show Must Go On

--- ns_stick_ino/ns_stick_ino.ino
+++ ns_stick_ino/ns_stick_ino.ino
@@ -6,6 +6,7 @@
 const int buttonPin = 2;
 int buttonState = 0;
 boolean running = false;
+String incomingByte = "";
 
 NS_Rainbow ns_stick = NS_Rainbow(N_CELL,PIN);
 void setup() {
@@ -13,55 +14,50 @@ void setup() {
   // initialize serial:
   Serial.begin(9600);
   delay(100);
+  Serial.setTimeout(10);
   ns_stick.begin();
 
   ns_stick.setBrightness(64);
 }
 
 void loop() {
+  incomingByte = "";
 
   // read data from pushbutton
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH && !running) {
     Serial.println("BTN,OPEN");
-    // ns_stick.setColor(0, 0, 255, 0);
-    // ns_stick.show();
     running = true;
   } if (buttonState != HIGH && running) {
-    Serial.println("BTN,CLOSE");
-    // ns_stick.setColor(0, 255, 0, 0);
-    // ns_stick.show();
+    Serial.println("BTN,CLOSED");
+    running = false;
   }
 
   // if there's any serial available, read it:
   while (Serial.available() > 0) {
 
-    // look for the next valid integer in the incoming serial stream:
-    int led = Serial.parseInt();
-    int red = Serial.parseInt();
-    // do it again:
-    int green = Serial.parseInt();
-    // do it again:
-    int blue = Serial.parseInt();
+    incomingByte = Serial.readString();
 
-    // look for the newline. That's the end of your
-    // sentence:
-    if (Serial.read() == '\n') {
-      // constrain the values to 0 - 255 and invert
-      // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
-      led = led;
-      red = constrain(red, 0, 255);
-      green = constrain(green, 0, 255);
-      blue = constrain(blue, 0, 255);
+    Serial.println(incomingByte);
+    // look for the next valid integer in the incoming serial stream:
+    // String ledS = incomingByte.substring(0, 1);
+    int led = incomingByte.substring(0, 1).toInt();
+
+    // String redS = incomingByte.substring(1, 4);
+    int red = incomingByte.substring(1, 4).toInt();
+
+    // do it again:
+    // String greenS = incomingByte.substring(4, 7);
+    int green = incomingByte.substring(4, 7).toInt();
+
+    // do it again:
+    // String blueS = incomingByte.substring(7, 10);
+    int blue = incomingByte.substring(7, 10).toInt();
 
       ns_stick.setColor(led, red, green, blue);
       ns_stick.show();
       // print the three numbers in one string as hexadecimal:
-      Serial.print(led);
-      Serial.print(" : ");
-      Serial.print(red);
-      Serial.print(green);
-      Serial.println(blue);
-    }
+      Serial.println(led);
+
   }
 }
